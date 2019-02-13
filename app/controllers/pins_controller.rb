2612f6f21544e18d5569b1bea7fbd2f3956eda79
Rob Brown <brown4homes@yahoo.com>
2019-02-13 13:50:03 -0800

    Add CRUD functionality and views to Pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,11 @@
 class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
 
   def index
+
+  end
+
+  def show
 
   end
 
@@ -10,12 +15,22 @@ class PinsController < ApplicationController
 
   def create
     @pin = Pin.new(pin_params)
+
+    if @pin.save
+      redirect_to @pin, notice: "You have successfully created new Pin"
+    else
+      render 'new'
+    end
   end
 
   private
 
   def pin_params
     params.require(:pin).permit(:title, :description)
+  end
+
+  def find_pin
+    @pin = Pin.find(params[:id])
   end
 
 end
