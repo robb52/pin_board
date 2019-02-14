13e85a66b5abbaf9c08d405997b8f61df3cbfc39
Rob Brown <brown4homes@yahoo.com>
2019-02-13 17:37:09 -0800

    Added paperclip gem and images to pins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_000512) do
+ActiveRecord::Schema.define(version: 2019_02_14_005323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 2019_02_14_000512) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
